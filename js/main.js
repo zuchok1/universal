39c494123d0ff50100ebbeca39cc5fb8587c4ad4
zuchok1 <zuchok228@gmail.com>
2021-07-02 21:14:16 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const mySwiper = new Swiper('.news-tabs__swiper-container', {
   direction: 'vertical',
   loop: true,
+  speed: 400,
 
   //Pagination
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+
+  autoplay: {
+    delay: 5000,
   },
 })
